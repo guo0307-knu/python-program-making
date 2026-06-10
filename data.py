bf80a375bf8d44c5f59c034728c7e8f293cd74da
guo0307-knu <guo0307@knu.ac.kr>
2026-06-11 00:21:48 +0900

    Add files via upload

---
+++ data.py
@@ -0,0 +1,557 @@
+EvenData = {
+    #Table a-5
+    "Aluminum": {
+        "Modulus of Elasticity E (Mpsi)": 10.4,
+        "Modulus of Elasticity E (GPa)": 71.7,
+        "Modulus of Rigidity G (Mpsi)": 3.9,
+        "Modulus of Rigidity G (GPa)": 26.9,
+        "Poisson's Ratio": 0.333,
+        "Unit Weight w (lbf/in³)": 0.098,
+        "Unit Weight w (lbf/ft³)": 169,
+        "Unit Weight w (kN/m³)": 26.6
+    },
+    "Beryllium copper": {
+        "Modulus of Elasticity E (Mpsi)": 18.0,
+        "Modulus of Elasticity E (GPa)": 124.0,
+        "Modulus of Rigidity G (Mpsi)": 7.0,
+        "Modulus of Rigidity G (GPa)": 48.3,
+        "Poisson's Ratio": 0.285,
+        "Unit Weight w (lbf/in³)": 0.297,
+        "Unit Weight w (lbf/ft³)": 513,
+        "Unit Weight w (kN/m³)": 80.6
+    },
+    "Brass": {
+        "Modulus of Elasticity E (Mpsi)": 15.4,
+        "Modulus of Elasticity E (GPa)": 106.0,
+        "Modulus of Rigidity G (Mpsi)": 5.82,
+        "Modulus of Rigidity G (GPa)": 40.1,
+        "Poisson's Ratio": 0.324,
+        "Unit Weight w (lbf/in³)": 0.309,
+        "Unit Weight w (lbf/ft³)": 534,
+        "Unit Weight w (kN/m³)": 83.8
+    },
+    "Carbon steel": {
+        "Modulus of Elasticity E (Mpsi)": 30.0,
+        "Modulus of Elasticity E (GPa)": 207.0,
+        "Modulus of Rigidity G (Mpsi)": 11.5,
+        "Modulus of Rigidity G (GPa)": 79.3,
+        "Poisson's Ratio": 0.292,
+        "Unit Weight w (lbf/in³)": 0.282,
+        "Unit Weight w (lbf/ft³)": 487,
+        "Unit Weight w (kN/m³)": 76.5
+    },
+    "Cast iron": {
+        "Modulus of Elasticity E (Mpsi)": 14.5,
+        "Modulus of Elasticity E (GPa)": 100.0,
+        "Modulus of Rigidity G (Mpsi)": 6.0,
+        "Modulus of Rigidity G (GPa)": 41.4,
+        "Poisson's Ratio": 0.211,
+        "Unit Weight w (lbf/in³)": 0.260,
+        "Unit Weight w (lbf/ft³)": 450,
+        "Unit Weight w (kN/m³)": 70.6
+    },
+    "Copper": {
+        "Modulus of Elasticity E (Mpsi)": 17.2,
+        "Modulus of Elasticity E (GPa)": 119.0,
+        "Modulus of Rigidity G (Mpsi)": 6.49,
+        "Modulus of Rigidity G (GPa)": 44.7,
+        "Poisson's Ratio": 0.326,
+        "Unit Weight w (lbf/in³)": 0.322,
+        "Unit Weight w (lbf/ft³)": 556,
+        "Unit Weight w (kN/m³)": 87.3
+    },
+    "Douglas fir": {
+        "Modulus of Elasticity E (Mpsi)": 1.6,
+        "Modulus of Elasticity E (GPa)": 11.0,
+        "Modulus of Rigidity G (Mpsi)": 0.6,
+        "Modulus of Rigidity G (GPa)": 4.1,
+        "Poisson's Ratio": 0.33,
+        "Unit Weight w (lbf/in³)": 0.016,
+        "Unit Weight w (lbf/ft³)": 28,
+        "Unit Weight w (kN/m³)": 4.3
+    },
+    "Glass": {
+        "Modulus of Elasticity E (Mpsi)": 6.7,
+        "Modulus of Elasticity E (GPa)": 46.2,
+        "Modulus of Rigidity G (Mpsi)": 2.7,
+        "Modulus of Rigidity G (GPa)": 18.6,
+        "Poisson's Ratio": 0.245,
+        "Unit Weight w (lbf/in³)": 0.094,
+        "Unit Weight w (lbf/ft³)": 162,
+        "Unit Weight w (kN/m³)": 25.4
+    },
+    "Inconel": {
+        "Modulus of Elasticity E (Mpsi)": 31.0,
+        "Modulus of Elasticity E (GPa)": 214.0,
+        "Modulus of Rigidity G (Mpsi)": 11.0,
+        "Modulus of Rigidity G (GPa)": 75.8,
+        "Poisson's Ratio": 0.290,
+        "Unit Weight w (lbf/in³)": 0.307,
+        "Unit Weight w (lbf/ft³)": 530,
+        "Unit Weight w (kN/m³)": 83.3
+    },
+    "Lead": {
+        "Modulus of Elasticity E (Mpsi)": 5.3,
+        "Modulus of Elasticity E (GPa)": 36.5,
+        "Modulus of Rigidity G (Mpsi)": 1.9,
+        "Modulus of Rigidity G (GPa)": 13.1,
+        "Poisson's Ratio": 0.425,
+        "Unit Weight w (lbf/in³)": 0.411,
+        "Unit Weight w (lbf/ft³)": 710,
+        "Unit Weight w (kN/m³)": 111.5
+    },
+    "Magnesium": {
+        "Modulus of Elasticity E (Mpsi)": 6.5,
+        "Modulus of Elasticity E (GPa)": 44.8,
+        "Modulus of Rigidity G (Mpsi)": 2.4,
+        "Modulus of Rigidity G (GPa)": 16.5,
+        "Poisson's Ratio": 0.350,
+        "Unit Weight w (lbf/in³)": 0.065,
+        "Unit Weight w (lbf/ft³)": 112,
+        "Unit Weight w (kN/m³)": 17.6
+    },
+    "Molybdenum": {
+        "Modulus of Elasticity E (Mpsi)": 48.0,
+        "Modulus of Elasticity E (GPa)": 331.0,
+        "Modulus of Rigidity G (Mpsi)": 17.0,
+        "Modulus of Rigidity G (GPa)": 117.0,
+        "Poisson's Ratio": 0.307,
+        "Unit Weight w (lbf/in³)": 0.368,
+        "Unit Weight w (lbf/ft³)": 636,
+        "Unit Weight w (kN/m³)": 100.0
+    },
+    "Monel metal": {
+        "Modulus of Elasticity E (Mpsi)": 26.0,
+        "Modulus of Elasticity E (GPa)": 179.0,
+        "Modulus of Rigidity G (Mpsi)": 9.5,
+        "Modulus of Rigidity G (GPa)": 65.5,
+        "Poisson's Ratio": 0.320,
+        "Unit Weight w (lbf/in³)": 0.319,
+        "Unit Weight w (lbf/ft³)": 551,
+        "Unit Weight w (kN/m³)": 86.6
+    },
+    "Nickel silver": {
+        "Modulus of Elasticity E (Mpsi)": 18.5,
+        "Modulus of Elasticity E (GPa)": 127.0,
+        "Modulus of Rigidity G (Mpsi)": 7.0,
+        "Modulus of Rigidity G (GPa)": 48.3,
+        "Poisson's Ratio": 0.322,
+        "Unit Weight w (lbf/in³)": 0.316,
+        "Unit Weight w (lbf/ft³)": 546,
+        "Unit Weight w (kN/m³)": 85.8
+    },
+    "Nickel steel": {
+        "Modulus of Elasticity E (Mpsi)": 30.0,
+        "Modulus of Elasticity E (GPa)": 207.0,
+        "Modulus of Rigidity G (Mpsi)": 11.5,
+        "Modulus of Rigidity G (GPa)": 79.3,
+        "Poisson's Ratio": 0.291,
+        "Unit Weight w (lbf/in³)": 0.280,
+        "Unit Weight w (lbf/ft³)": 484,
+        "Unit Weight w (kN/m³)": 76.0
+    },
+    "Phosphor bronze": {
+        "Modulus of Elasticity E (Mpsi)": 16.1,
+        "Modulus of Elasticity E (GPa)": 111.0,
+        "Modulus of Rigidity G (Mpsi)": 6.0,
+        "Modulus of Rigidity G (GPa)": 41.4,
+        "Poisson's Ratio": 0.349,
+        "Unit Weight w (lbf/in³)": 0.295,
+        "Unit Weight w (lbf/ft³)": 510,
+        "Unit Weight w (kN/m³)": 80.1
+    },
+    "Stainless steel (18-8)": {
+        "Modulus of Elasticity E (Mpsi)": 27.6,
+        "Modulus of Elasticity E (GPa)": 190.0,
+        "Modulus of Rigidity G (Mpsi)": 10.6,
+        "Modulus of Rigidity G (GPa)": 73.1,
+        "Poisson's Ratio": 0.305,
+        "Unit Weight w (lbf/in³)": 0.280,
+        "Unit Weight w (lbf/ft³)": 484,
+        "Unit Weight w (kN/m³)": 76.0
+    },
+    "Titanium alloys": {
+        "Modulus of Elasticity E (Mpsi)": 16.5,
+        "Modulus of Elasticity E (GPa)": 114.0,
+        "Modulus of Rigidity G (Mpsi)": 6.2,
+        "Modulus of Rigidity G (GPa)": 42.4,
+        "Poisson's Ratio": 0.340,
+        "Unit Weight w (lbf/in³)": 0.160,
+        "Unit Weight w (lbf/ft³)": 276,
+        "Unit Weight w (kN/m³)": 43.4
+    },
+
+    "1006 HR": {
+        "UNS No.": "G10060",
+        "Tensile Strength (MPa)": 300,
+        "Tensile Strength (kpsi)": 43,
+        "Yield Strength (MPa)": 170,
+        "Yield Strength (kpsi)": 24,
+        "Elongation in 2 in (%)": 30,
+        "Reduction in Area (%)": 55,
+        "Brinell Hardness": 86
+    },
+    "1006 CD": {
+        "UNS No.": "G10060",
+        "Tensile Strength (MPa)": 330,
+        "Tensile Strength (kpsi)": 48,
+        "Yield Strength (MPa)": 280,
+        "Yield Strength (kpsi)": 41,
+        "Elongation in 2 in (%)": 20,
+        "Reduction in Area (%)": 45,
+        "Brinell Hardness": 95
+    },
+    "1010 HR": {
+        "UNS No.": "G10100",
+        "Tensile Strength (MPa)": 320,
+        "Tensile Strength (kpsi)": 47,
+        "Yield Strength (MPa)": 180,
+        "Yield Strength (kpsi)": 26,
+        "Elongation in 2 in (%)": 28,
+        "Reduction in Area (%)": 50,
+        "Brinell Hardness": 95
+    },
+    "1010 CD": {
+        "UNS No.": "G10100",
+        "Tensile Strength (MPa)": 370,
+        "Tensile Strength (kpsi)": 53,
+        "Yield Strength (MPa)": 300,
+        "Yield Strength (kpsi)": 44,
+        "Elongation in 2 in (%)": 20,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 105
+    },
+    "1015 HR": {
+        "UNS No.": "G10150",
+        "Tensile Strength (MPa)": 340,
+        "Tensile Strength (kpsi)": 50,
+        "Yield Strength (MPa)": 190,
+        "Yield Strength (kpsi)": 27.5,
+        "Elongation in 2 in (%)": 28,
+        "Reduction in Area (%)": 50,
+        "Brinell Hardness": 101
+    },
+    "1015 CD": {
+        "UNS No.": "G10150",
+        "Tensile Strength (MPa)": 390,
+        "Tensile Strength (kpsi)": 56,
+        "Yield Strength (MPa)": 320,
+        "Yield Strength (kpsi)": 47,
+        "Elongation in 2 in (%)": 18,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 111
+    },
+    "1018 HR": {
+        "UNS No.": "G10180",
+        "Tensile Strength (MPa)": 400,
+        "Tensile Strength (kpsi)": 58,
+        "Yield Strength (MPa)": 220,
+        "Yield Strength (kpsi)": 32,
+        "Elongation in 2 in (%)": 25,
+        "Reduction in Area (%)": 50,
+        "Brinell Hardness": 116
+    },
+    "1018 CD": {
+        "UNS No.": "G10180",
+        "Tensile Strength (MPa)": 440,
+        "Tensile Strength (kpsi)": 64,
+        "Yield Strength (MPa)": 370,
+        "Yield Strength (kpsi)": 54,
+        "Elongation in 2 in (%)": 15,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 126
+    },
+    "1020 HR": {
+        "UNS No.": "G10200",
+        "Tensile Strength (MPa)": 380,
+        "Tensile Strength (kpsi)": 55,
+        "Yield Strength (MPa)": 210,
+        "Yield Strength (kpsi)": 30,
+        "Elongation in 2 in (%)": 25,
+        "Reduction in Area (%)": 50,
+        "Brinell Hardness": 111
+    },
+    "1020 CD": {
+        "UNS No.": "G10200",
+        "Tensile Strength (MPa)": 470,
+        "Tensile Strength (kpsi)": 68,
+        "Yield Strength (MPa)": 390,
+        "Yield Strength (kpsi)": 57,
+        "Elongation in 2 in (%)": 15,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 131
+    },
+    "1030 HR": {
+        "UNS No.": "G10300",
+        "Tensile Strength (MPa)": 470,
+        "Tensile Strength (kpsi)": 68,
+        "Yield Strength (MPa)": 260,
+        "Yield Strength (kpsi)": 37.5,
+        "Elongation in 2 in (%)": 20,
+        "Reduction in Area (%)": 42,
+        "Brinell Hardness": 137
+    },
+    "1030 CD": {
+        "UNS No.": "G10300",
+        "Tensile Strength (MPa)": 520,
+        "Tensile Strength (kpsi)": 76,
+        "Yield Strength (MPa)": 440,
+        "Yield Strength (kpsi)": 64,
+        "Elongation in 2 in (%)": 12,
+        "Reduction in Area (%)": 35,
+        "Brinell Hardness": 149
+    },
+    "1035 HR": {
+        "UNS No.": "G10350",
+        "Tensile Strength (MPa)": 500,
+        "Tensile Strength (kpsi)": 72,
+        "Yield Strength (MPa)": 270,
+        "Yield Strength (kpsi)": 39.5,
+        "Elongation in 2 in (%)": 18,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 143
+    },
+    "1035 CD": {
+        "UNS No.": "G10350",
+        "Tensile Strength (MPa)": 550,
+        "Tensile Strength (kpsi)": 80,
+        "Yield Strength (MPa)": 460,
+        "Yield Strength (kpsi)": 67,
+        "Elongation in 2 in (%)": 12,
+        "Reduction in Area (%)": 35,
+        "Brinell Hardness": 163
+    },
+    "1040 HR": {
+        "UNS No.": "G10400",
+        "Tensile Strength (MPa)": 520,
+        "Tensile Strength (kpsi)": 76,
+        "Yield Strength (MPa)": 290,
+        "Yield Strength (kpsi)": 42,
+        "Elongation in 2 in (%)": 18,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 149
+    },
+    "1040 CD": {
+        "UNS No.": "G10400",
+        "Tensile Strength (MPa)": 590,
+        "Tensile Strength (kpsi)": 85,
+        "Yield Strength (MPa)": 490,
+        "Yield Strength (kpsi)": 71,
+        "Elongation in 2 in (%)": 12,
+        "Reduction in Area (%)": 35,
+        "Brinell Hardness": 170
+    },
+    "1045 HR": {
+        "UNS No.": "G10450",
+        "Tensile Strength (MPa)": 570,
+        "Tensile Strength (kpsi)": 82,
+        "Yield Strength (MPa)": 310,
+        "Yield Strength (kpsi)": 45,
+        "Elongation in 2 in (%)": 16,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 163
+    },
+    "1045 CD": {
+        "UNS No.": "G10450",
+        "Tensile Strength (MPa)": 630,
+        "Tensile Strength (kpsi)": 91,
+        "Yield Strength (MPa)": 530,
+        "Yield Strength (kpsi)": 77,
+        "Elongation in 2 in (%)": 12,
+        "Reduction in Area (%)": 35,
+        "Brinell Hardness": 179
+    },
+    "1050 HR": {
+        "UNS No.": "G10500",
+        "Tensile Strength (MPa)": 620,
+        "Tensile Strength (kpsi)": 90,
+        "Yield Strength (MPa)": 340,
+        "Yield Strength (kpsi)": 49.5,
+        "Elongation in 2 in (%)": 15,
+        "Reduction in Area (%)": 35,
+        "Brinell Hardness": 179
+    },
+    "1050 CD": {
+        "UNS No.": "G10500",
+        "Tensile Strength (MPa)": 690,
+        "Tensile Strength (kpsi)": 100,
+        "Yield Strength (MPa)": 580,
+        "Yield Strength (kpsi)": 84,
+        "Elongation in 2 in (%)": 10,
+        "Reduction in Area (%)": 30,
+        "Brinell Hardness": 197
+    },
+    "1060 HR": {
+        "UNS No.": "G10600",
+        "Tensile Strength (MPa)": 680,
+        "Tensile Strength (kpsi)": 98,
+        "Yield Strength (MPa)": 370,
+        "Yield Strength (kpsi)": 54,
+        "Elongation in 2 in (%)": 12,
+        "Reduction in Area (%)": 30,
+        "Brinell Hardness": 201
+    },
+    "1080 HR": {
+        "UNS No.": "G10800",
+        "Tensile Strength (MPa)": 770,
+        "Tensile Strength (kpsi)": 112,
+        "Yield Strength (MPa)": 420,
+        "Yield Strength (kpsi)": 61.5,
+        "Elongation in 2 in (%)": 10,
+        "Reduction in Area (%)": 25,
+        "Brinell Hardness": 229
+    },
+    "1095 HR": {
+        "UNS No.": "G10950",
+        "Tensile Strength (MPa)": 830,
+        "Tensile Strength (kpsi)": 120,
+        "Yield Strength (MPa)": 460,
+        "Yield Strength (kpsi)": 66,
+        "Elongation in 2 in (%)": 10,
+        "Reduction in Area (%)": 25,
+        "Brinell Hardness": 248
+    },
+
+    #Table 21
+    "1030 Q&T 205C": {
+        "Treatment": "Q&T*",
+        "Temperature (C)": 205,
+        "Tensile Strength (MPa)": 848,
+        "Tensile Strength (kpsi)": 123,
+        "Yield Strength (MPa)": 648,
+        "Yield Strength (kpsi)": 94,
+        "Elongation (%)": 17,
+        "Reduction in Area (%)": 47,
+        "Brinell Hardness": 495
+    },
+    "1030 Q&T 315C": {
+        "Treatment": "Q&T*",
+        "Temperature (C)": 315,
+        "Tensile Strength (MPa)": 800,
+        "Tensile Strength (kpsi)": 116,
+        "Yield Strength (MPa)": 621,
+        "Yield Strength (kpsi)": 90,
+        "Elongation (%)": 19,
+        "Reduction in Area (%)": 53,
+        "Brinell Hardness": 401
+    },
+    "1030 Normalized": {
+        "Treatment": "Normalized",
+        "Temperature (C)": 925,
+        "Tensile Strength (MPa)": 521,
+        "Tensile Strength (kpsi)": 75,
+        "Yield Strength (MPa)": 345,
+        "Yield Strength (kpsi)": 50,
+        "Elongation (%)": 32,
+        "Reduction in Area (%)": 61,
+        "Brinell Hardness": 149
+    },
+    "1030 Annealed": {
+        "Treatment": "Annealed",
+        "Temperature (C)": 870,
+        "Tensile Strength (MPa)": 430,
+        "Tensile Strength (kpsi)": 62,
+        "Yield Strength (MPa)": 317,
+        "Yield Strength (kpsi)": 46,
+        "Elongation (%)": 35,
+        "Reduction in Area (%)": 64,
+        "Brinell Hardness": 137
+    },
+
+    # --- AISI 1040 ---
+    "1040 Q&T 205C": {
+        "Treatment": "Q&T",
+        "Temperature (C)": 205,
+        "Tensile Strength (MPa)": 779,
+        "Tensile Strength (kpsi)": 113,
+        "Yield Strength (MPa)": 593,
+        "Yield Strength (kpsi)": 86,
+        "Elongation (%)": 19,
+        "Reduction in Area (%)": 48,
+        "Brinell Hardness": 262
+    },
+    "1040 Normalized": {
+        "Treatment": "Normalized",
+        "Temperature (C)": 900,
+        "Tensile Strength (MPa)": 590,
+        "Tensile Strength (kpsi)": 86,
+        "Yield Strength (MPa)": 374,
+        "Yield Strength (kpsi)": 54,
+        "Elongation (%)": 28,
+        "Reduction in Area (%)": 55,
+        "Brinell Hardness": 170
+    },
+
+    # --- AISI 1050 ---
+    "1050 Q&T 205C": {
+        "Treatment": "Q&T*",
+        "Temperature (C)": 205,
+        "Tensile Strength (MPa)": 1120,
+        "Tensile Strength (kpsi)": 163,
+        "Yield Strength (MPa)": 807,
+        "Yield Strength (kpsi)": 117,
+        "Elongation (%)": 9,
+        "Reduction in Area (%)": 27,
+        "Brinell Hardness": 514
+    },
+    "1050 Annealed": {
+        "Treatment": "Annealed",
+        "Temperature (C)": 790,
+        "Tensile Strength (MPa)": 636,
+        "Tensile Strength (kpsi)": 92,
+        "Yield Strength (MPa)": 365,
+        "Yield Strength (kpsi)": 53,
+        "Elongation (%)": 24,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 187
+    },
+
+    # --- AISI 4130 (Table 21 - Continued) ---
+    "4130 Q&T 205C": {
+        "Treatment": "Q&T*",
+        "Temperature (C)": 205,
+        "Tensile Strength (MPa)": 1630,
+        "Tensile Strength (kpsi)": 236,
+        "Yield Strength (MPa)": 1460,
+        "Yield Strength (kpsi)": 212,
+        "Elongation (%)": 10,
+        "Reduction in Area (%)": 41,
+        "Brinell Hardness": 467
+    },
+    "4130 Normalized": {
+        "Treatment": "Normalized",
+        "Temperature (C)": 870,
+        "Tensile Strength (MPa)": 670,
+        "Tensile Strength (kpsi)": 97,
+        "Yield Strength (MPa)": 436,
+        "Yield Strength (kpsi)": 63,
+        "Elongation (%)": 25,
+        "Reduction in Area (%)": 59,
+        "Brinell Hardness": 197
+    },
+
+    # --- AISI 4340 (Table 21 - Continued) ---
+    "4340 Q&T 315C": {
+        "Treatment": "Q&T",
+        "Temperature (C)": 315,
+        "Tensile Strength (MPa)": 1720,
+        "Tensile Strength (kpsi)": 250,
+        "Yield Strength (MPa)": 1590,
+        "Yield Strength (kpsi)": 230,
+        "Elongation (%)": 10,
+        "Reduction in Area (%)": 40,
+        "Brinell Hardness": 486
+    },
+    "4340 Q&T 650C": {
+        "Treatment": "Q&T",
+        "Temperature (C)": 650,
+        "Tensile Strength (MPa)": 965,
+        "Tensile Strength (kpsi)": 140,
+        "Yield Strength (MPa)": 855,
+        "Yield Strength (kpsi)": 124,
+        "Elongation (%)": 19,
+        "Reduction in Area (%)": 60,
+        "Brinell Hardness": 280
+    },
+    
+}
